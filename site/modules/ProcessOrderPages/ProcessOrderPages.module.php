2c3520c51d250cd1e9a560347af582d99373f44f
Paul Ashby <paul@primitive.co>
2020-06-24 06:09:21 +0100

    Move cart to its own module at https://github.com/paulashby/OrderCart.git

--- site/modules/ProcessOrderPages/ProcessOrderPages.module.php
+++ site/modules/ProcessOrderPages/ProcessOrderPages.module.php
@@ -54,22 +54,7 @@ class ProcessOrderPages extends Process {
     $page_maker = $modules->get("PageMaker");
     $configured = array_key_exists("configured", $data);
 
-    if($configured){
-      
-      // We don't want to change anything once installed, so show warning and submit existing data if anything has been changed
-      if($this->configDiff($data)) {
-      
-        $this->session->warning("Names cannot be changed after installation. If you really need to rename, you can reinstall the module, but be aware that this will mean losing the order data currently in the system");
-        $curr_config = $this->modules->getConfig("PageMaker");
-        $event->arguments(1, $curr_config);
-
-      } else {
-        
-        // All good since we're changing nothing
-        $event->arguments(1, $data);
-
-      }
-    } else {
+    if( ! $configured) {
      
       // Just installed module
       $data["configured"] = true; // Set flag
@@ -140,12 +125,13 @@ class ProcessOrderPages extends Process {
         }
       }
       $event->arguments(1, $data);
-    }   
+    } else if($this->configDiff($data)) {
+      
+        // We don't want to change anything once installed, so show warning and submit existing data if anything has been changed
+        $this->session->warning("Names cannot be changed after installation. If you really need to rename, you can reinstall the module, but be aware that this will mean losing the order data currently in the system");
+
+      }  
   }
-
-
-
-
 /**
  * Custom uninstall 
  * 
@@ -188,13 +174,14 @@ class ProcessOrderPages extends Process {
  * @return Boolean - do common config entries differ?
  */
   public function configDiff($new_config) {
-
+bd($new_config);
     $curr_config = $this->modules->getConfig($this->className);
+    bd($curr_config);
      foreach ($new_config as $input_key => $input_val) {
-      
+      bd($this[$input_key]);
       if(array_key_exists($input_key, $curr_config)){
 
-          if($new_config[$config_key] !== $curr_config[$config_key]){
+          if($new_config[$input_key] !== $curr_config[$input_key]){
             return true;
           }
         }
@@ -369,300 +356,5 @@ class ProcessOrderPages extends Process {
       );
     }
     return $table_rows;
-  }
-// ----- Cart --------------------------------------------------------------------------
-/**
- * Add product to cart (creates a line-item page as child of /processwire/orders/cart-items)
- *
- * @param string $item The submitted form
- * @return string The configured field name
- */
-  public function addToCart($item) {
-    
-    $sku = $this->sanitizer->text($item->sku);
-    $new_quantity = $this->sanitizer->int($item->quantity);
-    
-    // Is there an existing order for this product?
-    $f_customer = $this["f_customer"];
-    $f_sku_ref = $this["f_sku_ref"];
-    $user_id = $this->users->getCurrentUser()->id;
-    $parent_selector = $this->getCartURL();
-    $child_selector = "$f_customer=$user_id,$f_sku_ref=$sku";
-    $exists_in_cart = $this->pages->get($parent_selector)->child($child_selector);
-
-    if($exists_in_cart->id) {
-      
-      // Add to existing item
-      $sum = $new_quantity + $exists_in_cart[$this["f_quantity"]];
-      $exists_in_cart->of(false);
-      $exists_in_cart->set($this["f_quantity"], $sum);
-      $exists_in_cart->save();
-
-    } else { 
-
-      // Create a new item
-      $item_title = $sku . ": " . $this->users->get($user_id)[$this["f_display_name"]];
-      $item_data = array("title" => $item_title);
-      $item_data[$this["f_customer"]] = $user_id;
-      $item_data[$this["f_sku_ref"]] = $sku;
-      $item_data[$this["f_quantity"]] = $new_quantity;
-
-      $cart_item = $this->wire("pages")->add($this["t_line-item"],  $this->getCartURL(), $item_data);
-    }
-    return json_encode(array("success"=>true));
-  }
-/**
- * Process line items, creating new order in /processwire/orders/pending-orders/
- *
- * @return Json
- */
-  public function placeOrder() {
-
-    // Get the parent page for the new order
-    $errors = array();
-
-    $orders_parent = $this->getOrdersPage("pending", $this->users->getCurrentUser()->id);
-    
-    if($orders_parent) {
-
-      $order_number = $this->getOrderNum();
-
-      // Create the order
-      $order_page = $this-> makePage($order_number, array("template" => "t_order", "parent"=>$orders_parent->path(), "title"=>$order_number));
-      $cart_items = $this->getCartItems();
-
-      foreach ($cart_items as $item) {
-        $item->of(false);
-        $item->parent = $order_page;
-        $item->save();
-      }
-      return json_encode(array("success"=>true));
-    }
-    $errors[] = "The orders page could not be found";
-    return json_encode(array("errors"=>$errors));
-  }
-/**
- * Move order to next step to reflect new status
- *
- * @param string $order_num
- * @param string $order_step
- * @return boolean
- */
-  protected function progressOrder($order_num, $order_step) {
-    $order_selector = "template=" . $this["t_order"] . ",name={$order_num}";
-    $admin_url = $this->config->url("admin");
-    $order_pg = $this->pages->findOne($order_selector);
-    
-    if($order_pg->id){
-      // Get the customer
-      $customer = $order_pg->children()->first()[$this["f_customer"]];
-      $next_step = $this->getOrdersPage($order_step, $customer);
-      $order_pg->of(false);
-      $order_pg->parent = $next_step;
-      $order_pg->save();
-      return true;
-    }
-    return false;
-  }
-/**
- * Get parent page for order - for current user only if id supplied
- *
- * @param string $order_step
- * @param integer $user_id
- * @return PageArray or Page
- */
-  protected function getOrdersPage($order_step, $user_id = null) {
-    
-    $admin_url = $this->config->url("admin");
-    $parent_path = $this["order_root"] . "/{$order_step}-orders/";
-    $parent_selector = "$parent_path,include=all"; 
-    $order_parent_name =  "{$user_id}_orders";
-    if($user_id) {
-      // User provided, so get the orders page just for this customer
-      $child_selector = "name=$order_parent_name,include=all";
-      $user_order_page = $this->pages->get($parent_selector)->child($child_selector);
-      if($user_order_page->id) {
-        return $user_order_page;
-      }
-      // No orders for this user - make a new page within pending orders
-      return $this->makePage($order_parent_name, array("template" => "t_user-orders", "parent"=>$parent_path, "title"=>$order_parent_name));
-    }
-    // All orders for given step
-    return $this->pages->get($parent_path)->children();
-  }
-  /**
- * Remove line item from cart
- *
- * @param string  $sku The item to remove
- * @return Json Updated cart markup if successful
- */
-  public function removeCartItem($sku) {
-    $cart_item = $this->getCartItem($sku);
-    if($cart_item->id) {
-      
-      $cart_item->delete(true);
-      return json_encode(array("success"=>true, "cart"=>$this->renderCart(true)));  
-    }
-    return json_encode(array("error"=>"The item could not be found"));
-  }
-/**
- * Change quantity of cart item
- *
- * @param string  $sku The item to update
- * @param string  $qty The new value
- * @return Json Updated cart markup if successful
- */
-  public function changeQuantity($sku, $qty) {
-
-    $cart_item = $this->getCartItem($sku);
-    $qtys = $this->sanitizer->text($qty);
-
-    if($cart_item->id) {
-        $cart_item->of(false);
-        $cart_item->set($this["f_quantity"], (int)$qtys);
-        $cart_item->save();
-        return json_encode(array("success"=>true, "cart"=>$this->renderCart(true)));  
-    }
-    return json_encode(array("error"=>"The item could not be found"));
-  }
-/**
- * Convert an integer representing GB pence to a GBP string 
- *
- * @param int $pence
- * @return string GBP value as a string with decimal point and prepended £
- */
-  public function renderPrice($pence) {
-
-    return "£" . number_format($pence/100, 2);
-  }
-/**
- * Generate HTML markup for current user's cart
- *
- * @param boolean $omitContainer - true if outer div not required (useful to avoid losing click handler)
- * @return string HTML markup
- */
-  public function renderCart($omitContainer = false) {
-
-    // Store field and template names in variables for markup
-    $f_sku = $this["f_sku"];
-    $f_sku_ref = $this["f_sku_ref"];
-    $f_quantity = $this["f_quantity"];
-    $open = $omitContainer ? "" : "<div class='cart-items'>";
-    $close = $omitContainer ? "" : "</div>";
-
-    $cart_items = $this->getCartItems();
-
-    $render = $open;
-    $render .= "<form class='cart-items__form' action='' method='post'>";
-    // cart_items are line_items NOT product pages
-    foreach ($cart_items as $item => $data) {
-      $sku_ref = $data[$f_sku_ref];
-      $sku_uc = strtoupper($sku_ref);
-      $product_selector = "template=product, {$f_sku}={$sku_ref}";
-      $product = $this->pages->findOne($product_selector);
-      $price = $this->renderPrice($product->price);
-      $quantity = $data[$f_quantity];
-      $subtotal = $this->renderPrice($product->price * $quantity);
-
-      $render .= "<fieldset class='form__fieldset'>
-      <legend>" . $product->title . "</legend>";
-      
-      $render .= "<p>SKU: {$sku_uc}</p>
-        <label class='form__label' for='quantity'>Quantity (Packs of 6):</label>
-        <input class='form__quantity' type='number' data-action='qtychange' data-sku='{$sku_ref}' name='quantity[]' min='1' step='1' value='{$quantity}'>
-        <p class='form__price'>Pack price: $price</p>
-        <p class='form__price--subtotal'>Subtotal: $subtotal</p>
-        <input type='hidden' name='sku[]' value='{$sku_ref}'>
-        <input type='button' class='form__button form__button--remove' value='Remove' data-action='remove' data-sku='{$sku_ref}'>
-        </fieldset>";
-    }
-    $render .= "<input class='form__button form__button--submit' type='submit' name='submit' value='submit'>
-      </form>";
-    $render .= $close;
-
-    return $render;
-  }
-/**
- * Get item from cart
- *
- * @param string  $sku The item to get
- * @return object Line item page or boolean false
- */
-  protected function getCartItem($sku) {
-
-    $skus = $this->sanitizer->text($sku);
-    $user_id = $this->users->getCurrentUser()->id;
-    $f_customer = $this["f_customer"];
-    $f_sku_ref = $this["f_sku_ref"];
-    $admin_url = $this->config->url("admin");
-    $parent_selector = "{$admin_url}orders/cart-items/, include=all";
-    $child_selector = "{$f_customer}={$user_id}, {$f_sku_ref}={$skus}, include=all";
-    $cart_item = $this->pages->findOne($parent_selector)->child($child_selector);
-
-    if($cart_item->id) {
-      return $cart_item;
-    }
-    return false;
-  }
-/**
- * Get all cart items for the current user
- *
- * @return wireArray The cart items
- */
-  protected function getCartItems() {
-    $user_id = $this->users->getCurrentUser()->id;
-    $admin_url = $this->config->url("admin");
-    $t_line_item = $this["t_line-item"];
-    $f_customer = $this["f_customer"];
-    return $this->pages->findOne($this["order_root"] . ", include=all")->children("template={$t_line_item}, {$f_customer}={$user_id}, include=all");
-  }
-/**
- * Get the path of the cart
- *
- * @return string
- */
-  public function getCartURL() {
-    return $this["order_root"] . "/cart-items/";
-  }
-/**
- * Get order number then increment in db
- *
- * @return  string The unincremented order number
- */
-  protected function getOrderNum() {
-
-    $data = $this->modules->getConfig("ProcessOrderPages");
-    $order_num = $this->sanitizer->text($data["order_num"]);
-    $this_order_num = $order_num;
-    $order_num++;
-    $data["order_num"] = $this->sanitizer->text($order_num);
-    $this->modules->saveConfig("ProcessOrderPages", $data);
-    return $this_order_num;
-  }
-/**
- * Set order number
- *
- * @param string  $val The number to base new orders on
- * @return boolean
- */
-  protected function setOrderNum($val) {
-
-    $data = $this->modules->getConfig("ProcessOrderPages");
-    $data["order_num"] = $val;
-    return $this->modules->saveConfig("ProcessOrderPages", $data);
-  }
-/**
- * Increment order number
- *
- * @return string The new order number
- */
-  protected function incrementOrderNum() {
-
-    $data = $this->modules->getConfig("ProcessOrderPages");
-    $order_num = $this->sanitizer->text($data["order_num"]);
-    $order_num++;
-    $data["order_num"] = $this->sanitizer->text($order_num);
-    $this->modules->saveConfig("ProcessOrderPages", $data);
-    return $data["order_num"];
   }
 }
