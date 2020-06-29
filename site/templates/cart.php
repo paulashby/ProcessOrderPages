3124aab13ba020c4b3ba68777230385ddf6a15f9
Paul Ashby <paul@primitive.co>
2020-06-30 00:58:19 +0100

    Use vanilla js for ajax calls. This allows us to bundle cart.js with the OrderCart module without worrying about dependency load orders

--- site/templates/cart.php
+++ site/templates/cart.php
@@ -2,32 +2,70 @@
 
 $cart = $this->modules->get("OrderCart");
 
-// Process checkout requests
-if($input->post->qtychange) {
+// Put this is cart
 
-	return $cart->changeQuantity($input->post->sku, $input->post->qtychange);
 
-} else if($input->post->remove) {
+if($config->ajax) {
 
-	return $cart->removeCartItem($input->post->sku);
+	$_input = file_get_contents("php://input");
 
-} else if($input->post->submit) {
+	if($_input) {
 
-	$cart->placeOrder();
+		$req = json_decode($_input);
+		$params = $req->params;
+
+		if($req->action === "qtychange") {
+
+			return $cart->changeQuantity($params->sku, $params->qty);
+		}
+
+		if($req->action === "remove") {
+
+			return $cart->removeCartItem($params->sku);
+		}
+
+		if($req->action === "submit") {
+
+			// From prev version - might need work
+			$cart->placeOrder();
+		}
+
+	} else {
+		return json_encode(array("success"=>false));
+	}
+
+
+
+	bd("ajax");
+	// Process checkout requests
+	if($input->post->qtychange) {
+		
+		return $cart->changeQuantity($input->post->sku, $input->post->qtychange);
+
+	} else if($input->post->remove) {
+
+		return $cart->removeCartItem($input->post->sku);
+
+	} else if($input->post->submit) {
+
+		$cart->placeOrder();
+	}
+} else {
+	bd("not ajax");
+	$out = "<!DOCTYPE html>
+	<html lang='en'>
+	<head>
+		<meta charset='UTF-8'>
+		<title>" . $page->title . "</title>";
+		// <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+	$out .= "</head>
+	<body>
+	<p><a href='" . $pages->get('/cards/')->url() . "'>Continue shopping</a></p>";
+		$out .= $cart->renderCart();
+		$out .= "
+		</body>
+	</html>";
+
+	echo $out;
+
 }
-
-$out = "<!DOCTYPE html>
-<html lang='en'>
-<head>
-	<meta charset='UTF-8'>
-	<title>" . $page->title . "</title>
-	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
-</head>
-<body>
-<p><a href='" . $pages->get('/cards/')->url() . "'>Continue shopping</a></p>";
-	$out .= $cart->renderCart();
-	$out .= "
-	</body>
-</html>";
-
-echo $out;
