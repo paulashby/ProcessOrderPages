f2a4b2ceffb5a22c567666b843b5a1c4bc9b3830
Paul Ashby <paul@primitive.co>
2020-07-01 09:00:17 +0100

    Update input processing to use PUT body. Get product listing markup from OrderCart renderItem method

--- site/templates/products.php
+++ site/templates/products.php
@@ -4,28 +4,20 @@ $cart = $this->modules->get("OrderCart");
 $settings = $this->modules->get("ProcessOrderPages");
 $sku_field = $settings['f_sku'];
 
-// Process 'add to cart' requests
-if($input->post->submit) {
-
-	if($user->isLoggedin()) {
-
-		$cart->addToCart($input->post);
-    }	
-}
-
 if($config->ajax) {
 
 	$_input = file_get_contents("php://input");
 
-	if($_input) {
+	if($_input && $user->isLoggedin()) {
 
 		$req = json_decode($_input);
-		$params = $req->params;
 
-		if($req->action === "qtychange") {
+		if(property_exists($req, "params")) {
+			$params = $req->params;
+		}
 
-			// return $cart->changeQuantity($params->sku, $params->qty);
-			$cart->addToCart($input->post);
+		if($req->action === "add") {
+			return $cart->addToCart($params->sku, $params->qty);
 		}
 
 	} else {
@@ -34,37 +26,28 @@ if($config->ajax) {
 
 } else {
 
-}
+	$out = "<!DOCTYPE html>
+	<html lang='en'>
+	<head>
+		<meta charset='UTF-8'>
+		<title>$page->title</title>
+	</head>
+	<body>
+	<p><a href='" . $pages->get('/cart/')->url() . "'>Cart</a></p>";
 
-$out = 
-"<!DOCTYPE html>
-<html lang='en'>
-<head>
-	<meta charset='UTF-8'>
-	<title>$page->title</title>
-</head>
-<body>
-<p><a href='" . $pages->get('/cart/')->url() . "'>Cart</a></p>";
+	$products = $pages->find('template=product');
 
-$products = $pages->find('template=product');
+	$out .= "<div class='products'>";
 
-$out .= "<div class='products'>";
+	foreach ($products as $product) {
+		$out .= $cart->renderItem($product);
+	}
 
-foreach ($products as $product) {
+	$out .= "</div>";	
+	$out .= "<script src='" . $this->config->urls->site . "modules/OrderCart/cart.js'></script>
+	</body>
+	</html>";
 
-	$out .= "<form action='' method='post'>
-		<h2>" . $product->title . "</h2>
-		<label class='.form__label' for='quantity'>Quantity (Packs of 6):</label>
-		<input class='.form__quantity' type='number' id='quantity' name='quantity' min='1' step='1' value='1'>
-		<input type='hidden' id='sku' name='sku' value='" . $product[$sku_field] . "'>
-		<input type='hidden' id='price' name='price' value='" . $product->price . "'>
-		  <input class='form__button form__button--submit' type='submit' name='submit' value='submit' data-action='submit'> 
-	</form>";
+	echo $out;
 
 }
-
-$out .= "</div>";	
-$out .= "</body>
-</html>";
-
-echo $out;
