5ddfa31b481335590919191143e2dcb66dd6092f
Paul Ashby <paul@primitive.co>
2020-07-01 08:23:20 +0100

    Update input processing to use PUT body

--- site/templates/cart.php
+++ site/templates/cart.php
@@ -2,62 +2,41 @@
 
 $cart = $this->modules->get("OrderCart");
 
-// Put this is cart
-
-
 if($config->ajax) {
 
 	$_input = file_get_contents("php://input");
 
-	if($_input) {
+	if($_input && $user->isLoggedin()) {
 
 		$req = json_decode($_input);
-		$params = $req->params;
 
-		if($req->action === "qtychange") {
+		if(property_exists($req, "params")) {
+			$params = $req->params;
+		}
 
+		if($req->action === "update") {
 			return $cart->changeQuantity($params->sku, $params->qty);
 		}
 
 		if($req->action === "remove") {
-
 			return $cart->removeCartItem($params->sku);
 		}
 
-		if($req->action === "submit") {
-
-			// From prev version - might need work
-			$cart->placeOrder();
+		if($req->action === "order") {
+			return $cart->placeOrder();
 		}
 
 	} else {
 		return json_encode(array("success"=>false));
 	}
 
-
-
-	bd("ajax");
-	// Process checkout requests
-	if($input->post->qtychange) {
-		
-		return $cart->changeQuantity($input->post->sku, $input->post->qtychange);
-
-	} else if($input->post->remove) {
-
-		return $cart->removeCartItem($input->post->sku);
-
-	} else if($input->post->submit) {
-
-		$cart->placeOrder();
-	}
 } else {
-	bd("not ajax");
+	
 	$out = "<!DOCTYPE html>
 	<html lang='en'>
 	<head>
 		<meta charset='UTF-8'>
 		<title>" . $page->title . "</title>";
-		// <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
 	$out .= "</head>
 	<body>
 	<p><a href='" . $pages->get('/cards/')->url() . "'>Continue shopping</a></p>";
